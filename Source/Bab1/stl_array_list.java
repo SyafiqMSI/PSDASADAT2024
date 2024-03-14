ccd2e7a91e2a85b8495144e57fd8a8683b6e0c7e
hanbee <117052559+AbrahamWillemH@users.noreply.github.com>
2024-03-14 15:06:37 +0700

    Update and rename stl_list.cpp to stl_array_list.java

---
+++ Source/Bab1/stl_array_list.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        // Mengisi ArrayList dengan elemen dari array a
+        for (int i : a) {
+            list1.add(i);
+        }
+
+        // Iterator maju
+        System.out.println("Iterator maju:");
+        for (int value : list1) {
+            System.out.print(value + " ");
+        }
+    }
+}
