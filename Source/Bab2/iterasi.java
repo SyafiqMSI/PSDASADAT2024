9d4732b6fe214456fd0fdba1248005ca107695f2
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2024-04-05 11:45:05 +0700

    Update : Cpp to Java

--- Source/Bab2/iterasi.java
+++ Source/Bab2/iterasi.java
@@ -1,3 +1,4 @@
+package Bab2;
 import java.util.Scanner;
 
 public class Iterasi {
---
+++ Source/Bab2/rekursi.java
@@ -0,0 +1,22 @@
+package Bab2;
+import java.util.Scanner;
+
+public class Rekursi {
+    public static int hitungFaktorial(int input) {
+        if (input > 1) {
+            return input * hitungFaktorial(input - 1);
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hitung Faktorial dengan Rekursi");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukkan angka:");
+        int angka = scanner.nextInt();
+
+        System.out.println(angka + "! = " + hitungFaktorial(angka));
+    }
+}
